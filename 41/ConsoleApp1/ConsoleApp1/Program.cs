7b72219e3f6219f160522f428f723a54a2030372
mariam rukhadze <mariamrukhadze10@gmail.com>
2023-10-29 17:35:25 +0400

    task 4

---
+++ 41/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 123, 456, 789 };
+
+        Console.WriteLine("Enter the index:");
+        if (int.TryParse(Console.ReadLine(), out int index))
+        {
+            int digitSum = SumOfDigitsAtIndex(numbers, index);
+
+            Console.WriteLine($"The sum of digits at index {index} is: {digitSum}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer for the index.");
+        }
+    }
+
+    static int SumOfDigitsAtIndex(int[] array, int index)
+    {
+        if (index < 0 || index >= array.Length)
+        {
+            throw new IndexOutOfRangeException("Index is out of range.");
+        }
+
+        int number = array[index];
+        int sum = 0;
+
+        string numberAsString = number.ToString();
+        foreach (char digitChar in numberAsString)
+        {
+            if (int.TryParse(digitChar.ToString(), out int digit))
+            {
+                sum += digit;
+            }
+        }
+
+        return sum;
+    }
+}
