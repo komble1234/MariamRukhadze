83fdbc9d174ed2bed025f6622328c8d1127f6901
mariam rukhadze <mariamrukhadze10@gmail.com>
2023-10-29 19:11:34 +0400

    finished

---
+++ 62/62/62/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public static class Calculator
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("Division by zero is not allowed.");
+        }
+
+        return a / b;
+    }
+}
